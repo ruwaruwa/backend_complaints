656684cdf80abf1cc4e736609dd8c0b7d2797e70
ruwaruwa <almozali287@gmail.com>
2024-03-06 13:13:40 +0300

    database updated

--- database/dbcng.js
+++ database/dbcng.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const dbCongration=async()=>{
     try {
-       await mongoose.connect('mongodb://localhost:27017/Oline_complaints')
+
+      //  await mongoose.connect('mongodb://localhost:27017/Oline_complaints')
+      await mongoose.connect('mongodb+srv://aruweyda45:12345@cluster0.4g4dxej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Oline_complaints')
        console.log('success fully connected') 
     } catch (error) {
       console.log(error)  
